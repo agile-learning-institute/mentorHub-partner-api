d6527e4d5178538d5d4402ee3d12bf96d4b2694b
Mallory O'Donnell <mallory.odonnell0723@gmail.com>
2024-02-21 10:37:42 -0500

    Connected to MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json()); //Middleware to parse JSON
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const mongoUrl = 'mongodb://root:example@localhost:27017/?tls=false&directConnection=true'; // Connection URL
+const client = new MongoClient(mongoUrl);
+
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (e) {
+    console.error('Failed to connect to MongoDB', e);
+  }
+}
+
+connectToMongoDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
     });
 
-    
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
