a0493b270c4627b45520bfba8276800d722ee7c6
Mike Storey <mike@agile-learning.institute>
2024-04-27 15:12:57 -0400

    houskeeping gitignore

--- dist/Handlers/helloWorldHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorldHandler = void 0;
-const helloWorldHandler = (req, res) => {
-    res.send('Hello, World!');
-};
-exports.helloWorldHandler = helloWorldHandler;
-exports.default = exports.helloWorldHandler;
