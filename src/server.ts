46a11600fda1c48946942f55e6531bdcf4d029d7
Mike Storey <mike@agile-learning.institute>
2024-05-02 14:35:40 -0400

    Implemented dependency injection in controllers

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
 import express from 'express';
 import Config from './config/Config'
-import PartnerRoutes from './routes/PartnerRoutes';
-import ConfigRoutes from './routes/ConfigRoutes';
+import PartnerController from './controllers/PartnerController';
+import ConfigController from './controllers/ConfigController';
 import MongoIO from './config/MongoIO'
 
-const app = express();
+// Initilize configurations and inject dependencie into controllers
 const config = new Config();
 const mongo = new MongoIO(config);
+const partnerController = new PartnerController(mongo);
+const configController = new ConfigController(config);
 
+const app = express();
 app.use(express.json());
-app.use('/api/partner', PartnerRoutes);
-app.use('/api/config', ConfigRoutes);
 
+// Map routes to controllers
+app.post('/api/partner/', (req, res) => partnerController.createPartner(req, res));
+app.get('/api/partner/', (req, res) => partnerController.getPartners(req, res));
+app.get('/api/partner/:partnerId', (req, res) => partnerController.getPartner(req, res));
+app.patch('/api/partner/:partnerId', (req, res) => partnerController.updatePartner(req, res));
+app.post('/api/partner/:partnerId/contact/:personId', (req, res) => partnerController.addContact(req, res));
+app.delete('/api/partner/:partnerId/contact/:personId', (req, res) => partnerController.removeContact(req, res));
+app.get('/api/config/', (req, res) => configController.getConfig(req, res));
+
+// Start Server
 const port = config.getPort();
 mongo.connect().then(() => {
     app.listen(port, () => {
