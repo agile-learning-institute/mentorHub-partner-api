3c188f51d21e0259a8b3718bfa20acc916e08b53
Mike Storey <mike@agile-learning.institute>
2024-05-02 14:36:37 -0400

    Implemented controller dependency injection

--- src/controllers/ConfigController.ts
+++ src/controllers/ConfigController.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
+import Config from '../config/Config';
 
-export const getConfig = async (req: Request, res: Response) => {
-  res.json({ message: 'Get Configurations' });
-};
+export default class ConfigController {
+  cfg: Config;
+
+  constructor(config: Config) {
+    this.cfg = config;
+  }
+
+  public getConfig = async (req: Request, res: Response) => {
+    res.json({ message: 'Get Configurations' });
+  };
+  
+}
--- src/controllers/PartnerController.ts
+++ src/controllers/PartnerController.ts
@@ -1,24 +1,33 @@
 import { Request, Response } from 'express';
+import MongoInterface from '../interfaces/MongoInterface';
 
-export const getPartners = async (req: Request, res: Response) => {
+export default class ConfigController {
+  mongo: MongoInterface;
+
+  constructor(mongoIO: MongoInterface) {
+    this.mongo = mongoIO;
+  }
+
+public getPartners = async (req: Request, res: Response) => {
   res.json({ message: 'Get Partner list' });
-};
+}
 
-export const getPartner = async (req: Request, res: Response) => {
+public getPartner = async (req: Request, res: Response) => {
   res.json({ message: 'Fetching partner' });
-};
+}
 
-export const createPartner = async (req: Request, res: Response) => {
+public createPartner = async (req: Request, res: Response) => {
   res.json({ message: 'Creating partner' });
-};
+}
 
-export const updatePartner = async (req: Request, res: Response) => {
+public updatePartner = async (req: Request, res: Response) => {
   res.json({ message: 'Update partner' });
-};
+}
 
-export const addContact = async (req: Request, res: Response) => {
+public addContact = async (req: Request, res: Response) => {
   res.json({ message: 'Add Contact to partner' });
-};
-export const removeContact = async (req: Request, res: Response) => {
+}
+public removeContact = async (req: Request, res: Response) => {
   res.json({ message: 'Remove Contact from partner' });
-};
+}
+}
