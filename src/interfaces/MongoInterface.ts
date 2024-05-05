29230789a06b521185fb9be2617ebd7ce3c3ab89
Mike Storey <mike@agile-learning.institute>
2024-05-04 21:37:54 -0400

    refactored to match instance

--- src/interfaces/MongoInterface.ts
+++ src/interfaces/MongoInterface.ts
@@ -1,15 +1,15 @@
 import ShortName from './ShortName';
-import Partner from './Partner';
-import { Contact } from './Contact';
+import { PartnerDoc } from '../models/PartnerModel';
+import { ContactDoc } from '../models/ContactModel';
 
 export default interface MongoInterface {
   connect(): Promise<void>
   disconnect(): Promise<void>;
   findPartners(): Promise<ShortName[]>;
-  findPartner(id: string): Promise<Partner>;
-  insertPartner(thePartner: Partner): Promise<Partner>;
-  updatePartner(id: string, data: any): Promise<Partner>;
-  addContact(partnerId: string, personId: string): Promise<Contact>;
+  findPartner(id: string): Promise<PartnerDoc>;
+  insertPartner(thePartner: PartnerDoc): Promise<PartnerDoc>;
+  updatePartner(id: string, data: any): Promise<PartnerDoc>;
+  addContact(partnerId: string, personId: string): Promise<ContactDoc>;
   removeContact(partnerId: string, personId: string): Promise<void>;
   loadVersions(): Promise<void>;
   loadEnumerators(collectionName: string): Promise<void>;
